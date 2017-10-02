4d703294a6b17edf911117ba86178be0e76cb3bf
ChenL <chenlinxh@gmail.com>
2017-10-02 13:46:39 +0800

    Improve tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,15 +1,7 @@
 <?php
-
-
-use Illuminate\Support\Str;
 use LumenPress\Nimble\Models\Post;
 use LumenPress\Nimble\Models\Taxonomy;
 use Illuminate\Database\Capsule\Manager as Capsule;
-
-$loader = require __DIR__.'/../vendor/autoload.php';
-$psr4 = require __DIR__.'/../vendor/composer/autoload_psr4.php';
-$path = dirname($psr4['LumenPress\\Nimble\\'][0]);
-$loader->addPsr4('LumenPress\\Nimble\\Tests\\', $path.'/tests');
 
 try {
     (new Dotenv\Dotenv(__DIR__.'/../'))->load();
@@ -17,19 +9,23 @@ try {
     //
 }
 
-date_default_timezone_set(getenv('APP_TIMEZONE'));
+date_default_timezone_set(env('APP_TIMEZONE', 'UTC'));
+
+$packagePath = realpath(dirname(PHPUNIT_COMPOSER_INSTALL).'/lumenpress/testing');
+
+system("php $packagePath/tests/includes/install.php");
 
 $capsule = new Capsule;
 
 $capsule->addConnection([
-    'driver'    => getenv('DB_DRIVER') ?: 'mysql',
-    'host'      => getenv('DB_HOST') ?: 'mysql',
-    'database'  => getenv('DB_NAME') ?: 'wordpress',
-    'username'  => getenv('DB_USER') ?: 'wordpress',
-    'password'  => getenv('DB_PASSWORD') === false ? 'wordpress' : getenv('DB_PASSWORD'),
+    'driver'    => 'mysql',
+    'host'      => env('DB_HOST', 'mysql'),
+    'database'  => env('DB_NAME', 'wordpress'),
+    'username'  => env('DB_USER', 'wordpress'),
+    'password'  => env('DB_PASSWORD', ''),
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
-    'prefix'    => getenv('DB_PREFIX') ?: 'wp_testing_',
+    'prefix'    => env('DB_PREFIX', 'wptests_'),
     'timezone'  => env('DB_TIMEZONE', '+00:00'),
     'strict'    => env('DB_STRICT_MODE', false),
 ]);
@@ -45,15 +41,6 @@ $capsule->setAsGlobal();
 
 // Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
 $capsule->bootEloquent();
-
-foreach (glob($path.'/tests/database/migrations/*.php') as $file) {
-    require_once $file;
-    $class = Str::studly(substr(basename($file, '.php'), 18));
-    $class = "LumenPress\Nimble\Tests\database\migrations\\".$class;
-    $migration = new $class;
-    $migration->down();
-    $migration->up();
-}
 
 Post::register('post', LumenPress\ACF\Tests\Models\Post::class);
 Post::register('page', LumenPress\ACF\Tests\Models\Page::class);
