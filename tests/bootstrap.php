c10922e2f147b7a7388fdfb21af746a87b9d9e5c
ChenL <chenlinxh@gmail.com>
2017-10-24 09:50:24 +0800

    update testing package

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,7 +10,19 @@ try {
     //
 }
 
-date_default_timezone_set(env('APP_TIMEZONE', 'UTC'));
+foreach ([
+    'APP_DEBUG' => true,
+    'DB_HOST' => 'localhost',
+    'DB_DATABASE' => 'wordpress',
+    'DB_USERNAME' => 'root',
+    'DB_PASSWORD' => '',
+    'DB_PREFIX' => 'wptests_',
+    'APP_TIMEZONE' => 'UTC',
+] as $key => $value) {
+    if (! getenv($key)) {
+        putenv("$key=$value");
+    }
+}
 
 $packagePath = realpath(dirname(PHPUNIT_COMPOSER_INSTALL).'/lumenpress/testing');
 
@@ -21,8 +33,8 @@ $capsule = new Capsule;
 $capsule->addConnection([
     'driver'    => 'mysql',
     'host'      => env('DB_HOST', 'mysql'),
-    'database'  => env('DB_NAME', 'wordpress'),
-    'username'  => env('DB_USER', 'wordpress'),
+    'database'  => env('DB_DATABASE', 'wordpress'),
+    'username'  => env('DB_USERNAME', 'wordpress'),
     'password'  => env('DB_PASSWORD', ''),
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
